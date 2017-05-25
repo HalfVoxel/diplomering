5638aaa9b60771ef832dff3a21457dadcac0f2c2
Aron Granberg <aron.granberg@gmail.com>
2017-05-25 19:39:12 +0200

    Improve run script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 dipl.py till_programmet.brief till_programmet.detailed < till_programmet.txt
+python3 dipl.py till_programmet.brief till_programmet.detailed
 g++ -std=c++11 -Wall -Wextra -O2 dipl.cpp
 cat config.in till_programmet.brief | ./a.out > groups.out
-python3 finalize.py till_programmet.txt.detailed < groups.out
+python3 finalize.py till_programmet.detailed < groups.out
